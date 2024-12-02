b638f44ade38b85b61f11cf24251698753194046
Giuseppe Calia <giuseppe.calia@aulab.it>
2024-12-02 21:02:47 +0100

    filtering and sorting

--- Lezione12 - Presto/public/js/announcements.js
+++ Lezione12 - Presto/public/js/announcements.js
@@ -20,10 +20,86 @@ function generateAnnoucementColumn(announcement) {
             <p class="mb-0"><i class="bi bi-bookmark-check-fill"></i> ${announcement.category}</p>
             <p class="mb-0"><i class="bi bi-calendar-fill"></i> ${date.toLocaleDateString()}</p>
         </div>
-    </div>
-    `;
+    </div>`;
 
     return col;
+}
+
+function populateAnnouncementsRow(announcements) {
+    const announcementsRow = document.getElementById('announcementsRow');
+
+    // cancello gli elementi contenuti all'interno di announcementsRow
+    // announcementsRow.innerHTML = '';
+    while(announcementsRow.hasChildNodes()) {
+        announcementsRow.removeChild(announcementsRow.firstChild)
+    }
+
+    announcements.forEach((announcement) => {
+        const col = generateAnnoucementColumn(announcement);
+        announcementsRow.appendChild(col);
+    });
+}
+
+function announcementsFilteringAndSorting(announcements, options) {
+
+    let announcementsFiltered = announcements.filter((announcement) => {
+        // console.log('sono nella funzione di callback di filter');
+        
+        let isAnnouncementRequired = true;
+
+        if(isAnnouncementRequired && options.search) {
+            isAnnouncementRequired = announcement.name.toLowerCase().includes(options.search.toLowerCase());
+        }
+
+        if(isAnnouncementRequired && options.category) {  // categorySelect.value != ''
+            isAnnouncementRequired = announcement.category == options.category;
+        }
+
+        if(isAnnouncementRequired && options.minPrice) {
+            isAnnouncementRequired = Number(options.minPrice) <= Number(announcement.price);
+        }
+
+        if(isAnnouncementRequired && options.maxPrice) {
+            isAnnouncementRequired = Number(options.maxPrice) >= Number(announcement.price);
+        }
+
+        return isAnnouncementRequired;
+    });
+
+    switch(options.sortedBy) {
+        case 'ascByDate':
+            announcementsFiltered.sort((left, right) => {
+                return left.createdAt - right.createdAt;
+            });
+            break;
+        case 'descByDate':
+            announcementsFiltered.sort((left, right) => {
+                return right.createdAt - left.createdAt;
+            });
+            break;
+        case 'ascByPrice':
+            announcementsFiltered.sort((left, right) => {
+                return Number(left.price) - Number(right.price);
+            });
+            break;
+        case 'descByPrice':
+            announcementsFiltered.sort((left, right) => {
+                return Number(right.price) - Number(left.price);
+            });
+            break;
+        case 'ascByAlpha':
+            announcementsFiltered.sort((left, right) => {
+                return left.name.toLowerCase().localeCompare(right.name.toLowerCase());
+            });
+            break;
+        case 'descByAlpha':
+            announcementsFiltered.sort((left, right) => {
+                return right.name.toLowerCase().localeCompare(left.name.toLowerCase());
+            });
+            break;
+    }
+
+    return announcementsFiltered;
 }
 
 async function loadAnnouncements() {
@@ -38,12 +114,31 @@ async function loadAnnouncements() {
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const announcementsRow = document.getElementById('announcementsRow');
-
     const announcements = await loadAnnouncements();
-    announcements.forEach((announcement) => {
 
-        const col = generateAnnoucementColumn(announcement);
-        announcementsRow.appendChild(col);
-    })
+    populateAnnouncementsRow(announcements);
+
+    const searchInput = document.getElementById('searchInput');
+    const categorySelect = document.getElementById('categorySelect');
+    const minPriceInput = document.getElementById('minPriceInput');
+    const maxPriceInput = document.getElementById('maxPriceInput');
+    const sortSelect = document.getElementById('sortSelect');
+
+    const filterForm = document.getElementById('filterForm');
+    filterForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const options = {
+            search: searchInput.value,
+            category: categorySelect.value,
+            minPrice: minPriceInput.value,
+            maxPrice: maxPriceInput.value,
+            sortedBy: sortSelect.value,
+        };
+
+        const announcementsFiltered = announcementsFilteringAndSorting(announcements, options);
+        populateAnnouncementsRow(announcementsFiltered);
+
+        // searchInput.value = ''
+    });
 });
